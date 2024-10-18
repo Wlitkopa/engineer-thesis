57fd8ab4c3bfb3dd0a01205e2e2ec4b9517fb2d6
koan <przemek@student.agh.edu.pl>
2024-10-18 14:27:03 +0200

    Calculated different Ns with different configuration of d and qd

--- regev_all.py
+++ regev_all.py
@@ -4,12 +4,12 @@ from utils.convert_to_matrix_row import convert_to_matrix_row
 from utils.convert_milliseconds import convert_milliseconds
 import time
 
-Ns = [15, 21, 33, 35, 39, 51, 55, 57, 65, 69, 77, 85, 91, 95, 119, 143]
-# Ns = [39, 51, 57]   Need to run this for ceil_floor (despite already calculated 39)
+# Ns = [15, 21, 33, 35, 39, 51, 55, 57, 65, 69, 77, 85, 91, 95, 119, 143]
+Ns = [39, 51, 57]   # Need to run this for ceil_floor (despite already calculated 39)
 # Ns = [15, 21, 33, 35, 39, 51, 55, 57, 65, 69, 77, 85, 91, 95, 119, 143]
 
-d_ceil_bool = False
-qd_ceil_bool = True
+d_ceil_bool = True
+qd_ceil_bool = False
 shots_num = 128
 
 for i in range(len(Ns)):
